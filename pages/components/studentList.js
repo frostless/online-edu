fe2f9afcd479937452b1f348ab1e1b6eab7f895f
wei zheng <tobe.zheng.wei@gmail.com>
2020-06-14 15:27:31 +1000

    format join time to "yyyy-mm-dd"

--- pages/components/studentList.js
+++ pages/components/studentList.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import API from '../lib/API'
+import Helper from '../lib/Helper'
 import { Table, Space } from 'antd';
 
 const columns = [
@@ -19,6 +20,12 @@ const columns = [
     dataIndex: 'area',
     key: 'area',
     width: '10%',
+    filters: [
+      { text: '加拿大', value: '加拿大' },
+      { text: '澳洲', value: '澳洲' },
+      { text: '国内', value: '国内' },
+    ],
+    onFilter: (value, record) => record.area.includes(value),
   },
   {
     title: 'Email',
@@ -265,7 +272,7 @@ const dataMapping = (input) => {
     obj["area"] = input[i].adress;
     obj["selectedCurriculum"] = input[i].course_name;
     obj["studentType"] = input[i].course_type;
-    obj["joinTime"] = input[i].update_date;
+    obj["joinTime"] = Helper.formatDate(input[i].update_date);
     data.push(obj);
   }
 
---
+++ pages/lib/Helper.js
@@ -0,0 +1,20 @@
+class Helper {
+  /**
+   * * @param {string} date the input date string
+   *
+   * @return {string} output date string in yyyy-mm-dd format
+   */
+  static formatDate(date) {
+      let d = new Date(date),
+      month = "" + (d.getMonth() + 1),
+      day = "" + d.getDate(),
+      year = d.getFullYear();
+
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+
+    return [year, month, day].join("-");
+  }
+}
+
+export default Helper;
