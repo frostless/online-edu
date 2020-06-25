1faa42f7d440bcd71ca405379fb773b9ecbf08d3
wei zheng <tobe.zheng.wei@gmail.com>
2020-06-25 22:50:47 +1000

    display course info

--- components/course/editcourse.js
+++ components/course/editcourse.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import API from "../../lib/api";
 import Notification from "../../lib/notification";
 import styled from "styled-components";
-import { Button, Input, Select } from 'antd';
+import { Form, Button, Input, Select } from "antd";
 const { Option } = Select;
 
 // Properties
@@ -11,132 +11,144 @@ const { Option } = Select;
 // Style Components
 // End Style Components
 
-const courseNameEmptyError = "Please ensure course name is not empty";
-const courseTypeInvalidError = "Please ensure course type is valid";
-const errorTitle = "Editing Course Error";
-const successTitle = "Editing Course Sucessfully";
+function EditCourse({id}) {
+  const [courseTypeList, setCourseTypeList] = useState([]);
+  const [courseName, setCourseName] = useState();
+  const [courseTypeID, setCourseTypeID] = useState();
+  const isNewCourse = !id;
 
-function EditCourse(props) {
-    const [courseTypeList, setCourseTypeList] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [courseTypeID, setCourseTypeID] = useState();
-    const [courseName, setCourseName] = useState();
-    const defaultCourseType = "Select From the List";
+  const addNewCourse = (course) => {
+    try {
+      API.addCourse(course);
+    } catch (e) {
+      return e;
+    }
+  };
 
-    const courseID = props['id'];
-    const isNewCourse = !courseID;
+  const updateCourse = (course) => {
+    try {
+      API.updateCourse(course);
+    } catch (e) {
+      return e;
+    }
+  };
 
-    const validatInput = () => {
-      if(!courseName)
-        return courseNameEmptyError;
-
-      if(!courseTypeID)
-        return courseTypeInvalidError;
+  const makCourse = (input) => {
+    let course = {
+      name: input["courseName"],
+      homework: "no",
+      type_id: input["courseType"],
     }
 
-    const addNewCourse = () =>{
-      try {
-        API.addCourse({
-          name: courseName,
-          homework: "no",
-          typeId: courseTypeID,
-        });
-      } catch (e) {
-        return e;
-      } 
+    if (!isNewCourse) 
+      course["id"] = id;
+
+    return course;
+  };
+
+  const processCourse = (input) => {
+    let response;
+    let course = makCourse(input);
+    if (isNewCourse) {
+      response = addNewCourse(course);
+    } else {
+      response = updateCourse(course);
+    }
+    return response;
+  };
+
+  const getSuccessTitle = () => {
+    return isNewCourse ? "Adding Course Successfully" : "Editing Course Successfully";
+  };
+
+  const getFailueTitle = () => {
+    return isNewCourse ? "Adding Course Failed" : "Editing Course Failed";
+  };
+
+  const getSuccessContent = (courseName) => {
+    return isNewCourse
+      ? `Course '${courseName}' has been added successfully`
+      : `Course '${courseName}' has been edited successfully`;
+  };
+
+  const onFinish = (input) => {
+    const {courseName} = input;
+    let error = processCourse(input);
+    if (error) {
+      Notification.notify(getFailueTitle(), error);
+      return;
     }
 
-    const updateCourse = () =>{
-      try {
-        API.updateCourse({
-          id: courseID,
-          name: courseName,
-          homework: "no",
-          typeId: courseTypeID,
-        });
-      } catch (e) {
-        return e;
-      } 
-    }
+    Notification.notify(
+      getSuccessTitle(),
+      getSuccessContent(courseName)
+    );
+  };
 
-    const processCourse = () => {
-      let response;
-      if(isNewCourse){
-        response =  addNewCourse();
-      } else {
-        response = updateCourse();
-      }
-      return response;
-    }
-
-    const onClick = () => {
-      let error = validatInput();
-      if (error) {
-        Notification.notify(errorTitle, error);
-        return;
-      }
-
-      error = processCourse();
-      if (error) {
-        Notification.notify(errorTitle, error);
-        return;
-      }
-
-      // Cleanup
-      setCourseTypeID(defaultCourseType);
-      setCourseName();
-      Notification.notify(successTitle, `Course '${courseName}' has been edited successfully`);
-    };
-
-    const onCourseTypeChange = (courseTypeID) => {
-        setCourseTypeID(courseTypeID);
-    };
-
-    const onCourseNameChange = (e) => {
-      const courseName = e.target.value;
-      setCourseName(courseName);
-    };
-
-    useEffect(() => {
-      API.getCourseTypeList().then((res) => {
-        let courseTypeList = [];
-        res.data.datas.forEach(element => {
-            const id = element['id'], name = element['name'];
-            const option = <Option key={id} value={id}>{name}</Option>
-            courseTypeList.push(option)
-        })
-        setCourseTypeList(courseTypeList);
-        setLoading(false);  
+  useEffect(() => {
+    API.getCourseTypeList().then((res) => {
+      let courseTypeList = [];
+      res.data.datas.forEach((element) => {
+        const id = element["id"],
+          name = element["name"];
+        const option = (
+          <Option key={id} value={id}>
+            {name}
+          </Option>
+        );
+        courseTypeList.push(option);
       });
-    }, []);
+      setCourseTypeList(courseTypeList);
+    });
+
+    if(isNewCourse)
+      return;
+
+    API.getCourse(id).then(res => {
+      const course = res.data.datas[0];
+      const {name, type_id} = course;
+      setCourseName(name)
+      setCourseTypeID(type_id);
+    })
+  }, [id]);
 
   return (
     <div style={{ width: "30%" }}>
-      <div>
-        <Input
-          placeholder="Course Name"
-          value={courseName}
-          onChange={onCourseNameChange}
-        />
-      </div>
-      <br />
-      <div>
-        <Select
-          defaultValue={defaultCourseType}
-          value={courseTypeID}
-          loading={loading}
-          onChange={onCourseTypeChange}
-          style={{ width: "100%" }}
+      <Form name="course" onFinish={onFinish}>
+        <Form.Item
+          name="courseName"
+          initialValue={courseName}
+          rules={[
+            {
+              required: true,
+              message: "Please enter course name!",
+            },
+          ]}
         >
-          {courseTypeList}
-        </Select>
-      </div>
-      <br />
-      <div>
-        <Button type="primary" onClick={onClick}>
-          Save Course
-        </Button>
-      </div>
+          <Input type="text" placeholder="Course Name" />
+        </Form.Item>
+        <Form.Item
+          name="courseType"
+          initialValue= "Please select from the list"
+          rules={[
+            {
+              required: true,
+              message: "Please select course type!",
+            },
+          ]}
+        >
+          <Select
+            style={{ width: "100%" }}
+          >
+            {courseTypeList}
+          </Select>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Save Course
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
