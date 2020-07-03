b5d9a84dfc68bb155d1f20ea560908d59af29c15
wei zheng <tobe.zheng.wei@gmail.com>
2020-07-03 22:11:26 +1000

    cleanup

--- __tests__/helper.unit.test.js
+++ __tests__/helper.unit.test.js
@@ -6,5 +6,11 @@ describe("Test Helper FormatDate Function", () => {
     expect(Helper.formatDate(input)).toBe("2020-06-19");
     input = "2020-06-30T16:00:00.000Z";
     expect(Helper.formatDate(input)).toBe("2020-06-30");
+    input = "Wed Mar 25 2015 11:00:00 GMT+1100";
+    expect(Helper.formatDate(input)).toBe("2015-03-25");
+    input = "03/25/2016"
+    expect(Helper.formatDate(input)).toBe("2016-03-25");
+    input = "2020/01/01"
+    expect(Helper.formatDate(input)).toBe("2020-01-01");
   });
 });
